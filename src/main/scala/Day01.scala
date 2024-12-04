4ba87a7ff0e76cf0942756864d321cbb3f8ba195
Joao Machado <13315199+machadoit@users.noreply.github.com>
2024-12-05 00:08:58 +0100

    1

--- src/main/scala/Day01.scala
+++ src/main/scala/Day01.scala
@@ -2,17 +2,35 @@ import Util.read
 
 @main def day01(): Unit = {
 
-  val input = read("resources/day01")()
+  val input = read("resources/day01")(_.toInt)
 
   // Part 1
 
-  val result1 = "foo"
+  val (leftL, rightL) = locations(input)
+  val distances = leftL.sorted.zip(rightL.sorted).map((l, r) => Math.abs(l - r))
+  val result1 = distances.sum
 
   println(result1)
 
   // Part 2
 
-  val result2 = "foo"
+  val rLookup = occurrenceLookup(rightL)
+
+  val result2 = leftL.foldLeft(0) { case (total, id) =>
+    total + (id * rLookup.getOrElse(id, 0))
+  }
 
   println(result2)
+}
+
+def locations(input: List[List[Int]]): (Seq[Int], Seq[Int]) = {
+  input.foldLeft((List.empty[Int], List.empty[Int])) { case ((leftL, rightL), left :: right :: Nil) =>
+    (left +: leftL, right +: rightL)
+  }
+}
+
+def occurrenceLookup(lst: Seq[Int]): Map[Int, Int] = {
+  lst.groupBy(identity).map { (key, values) =>
+    (key, values.length)
+  }
 }
